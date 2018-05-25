51b62f2c632fb742e3582e30ad05efe66d25873d
Daniel Hildebrandt <daniel.s.hildebrandt@gmail.com>
2018-05-24 23:07:58 -0700

    Mostly got debug compilation going, some issues with source files being spread across multiple directories.

--- test/clamped_numbers_test.cc
+++ test/clamped_numbers_test.cc
@@ -3,5 +3,13 @@
 
 namespace
 {
-  // Tests go here
+  using namespace clamped;
+  
+  TEST(BasicNumber, Getters)
+  {
+    BasicClampedNumber<int> num(2, -10, 10);
+    EXPECT_EQ(num.value(), 2);
+    EXPECT_EQ(num.minValue(), -10);
+    EXPECT_EQ(num.maxValue(), 10);
+  }
 }
